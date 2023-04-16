38beb24be13fc7f637e7c392ef1dfc8dfd1a4aba
Vojtech Simetka <vojtech@simetka.cz>
2023-04-16 16:39:50 +0200

    feat: add header, container, button and card components (#2)

---
+++ src/lib/card.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	export let image: string
+	export let title: string
+	export let description: string
+</script>
+
+<div class="root grow">
+	<div class="row">
+		<div class="img">
+			<img src={image} alt="waku object" />
+		</div>
+		<div class="column grow">
+			<div class="row">
+				<h2>{title}</h2>
+				<slot />
+			</div>
+			<span class="description">{description}</span>
+		</div>
+	</div>
+</div>
+
+<style>
+	.root {
+		border: 1px solid var(--color-border);
+		padding: var(--spacing-12);
+	}
+	.img {
+		background-color: #eee;
+		width: 60px;
+		height: 60px;
+		margin-right: var(--spacing-12);
+		flex-shrink: 0;
+	}
+	.description {
+		margin-top: var(--spacing-6);
+	}
+	.row {
+		display: flex;
+		flex-direction: row;
+	}
+	.column {
+		display: flex;
+		flex-direction: column;
+	}
+	.grow {
+		flex-grow: 1;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Add from '$lib/icons/add.svelte'
+	import ArrowLeft from '$lib/icons/arrow-left.svelte'
+
+	import Container from '$lib/container.svelte'
+	import Header from '$lib/header.svelte'
+	import Card from '$lib/card.svelte'
+	import Button from '$lib/button.svelte'
+
+	const cards = [
+		{
+			image: '',
+			title: 'Request transaction',
+			description: 'Receive funds from anyone in the chat to your wallet.',
+		},
+		{
+			image: '',
+			title: 'Send transaction',
+			description: 'Send funds to anyone in the chat from your wallet.',
+		},
+	]
+</script>
+
+<Header>
+	<Button slot="left" icon={ArrowLeft} border={false} />
+	<h1>Waku Objects</h1>
+</Header>
+
+<Container gap={12}>
+	{#each cards as card}
+		<Card {...card} />
+	{/each}
+
+	<Button icon={Add}>Add Waku Object</Button>
+</Container>
