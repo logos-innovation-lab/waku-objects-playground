59b0ae6e8891ce8eb59799dfa703ccac13475ce9
Vojtech Simetka <vojtech@simetka.cz>
2023-05-25 14:38:21 +0200

    feat: save user profile

--- src/routes/identity/+page.svelte
+++ src/routes/identity/+page.svelte
@@ -16,23 +16,31 @@
 	import { clipAndResize } from '$lib/utils/image'
 	import routes from '$lib/routes'
 
-	let picture = $profile.avatar
+	let avatar = $profile.avatar
 	let name = $profile.name
 
-	$: if ($profile.loading === false && !name && !picture) {
+	$: if ($profile.loading === false && !name && !avatar) {
 		name = $profile.name
-		picture = $profile.avatar
+		avatar = $profile.avatar
 	}
 
-	let pictureFiles: FileList | undefined = undefined
+	let files: FileList | undefined = undefined
 	async function resizePersonaPicture(p?: File) {
 		try {
-			picture = p ? await adapters.uploadPicture(await clipAndResize(p, 200, 200)) : picture
+			avatar = p ? await adapters.uploadPicture(await clipAndResize(p, 200, 200)) : avatar
 		} catch (error) {
 			console.error(error)
 		}
 	}
-	$: resizePersonaPicture(pictureFiles && pictureFiles[0])
+	$: resizePersonaPicture(files && files[0])
+
+	// Whenever profile name or avatar changes, save it
+	$: if (
+		!$profile.loading &&
+		((name && name !== $profile.name) || (avatar && avatar !== $profile.avatar))
+	) {
+		adapters.saveUserProfile(name, avatar)
+	}
 </script>
 
 <Header title="Account">
@@ -51,9 +59,9 @@
 {:else}
 	<Container gap={6}>
 		<div class="avatar">
-			{#if picture}
+			{#if avatar}
 				<div class="img">
-					<img src={adapters.getPicture(picture)} alt="profile" />
+					<img src={adapters.getPicture(avatar)} alt="your avatar" />
 				</div>
 			{:else}
 				<div class="no-img">
@@ -63,11 +71,11 @@
 				</div>
 			{/if}
 		</div>
-		<InputFile bind:files={pictureFiles}>
+		<InputFile bind:files>
 			<Renew />
 			Change picture
 		</InputFile>
-		<Textarea value={$profile.name} label="Display name" />
+		<Textarea bind:value={name} label="Display name" />
 	</Container>
 	<Divider pad={12} />
 	<Container gap={6}>
