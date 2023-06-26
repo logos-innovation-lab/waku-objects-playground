196f2e171ac5febef432f3bc2e01e7cb21ec45f6
Barbara Gomes <sugarpusher@users.noreply.github.com>
2023-06-26 15:49:46 -0400

    feat: create ButtonBlock component (#120)

---
+++ src/lib/components/button-block.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let borderTop = false
+	export let borderBottom = false
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	class={`block-btn ${borderTop ? 'borderTop' : ''} ${borderBottom ? 'borderBottom' : ''} `}
+	on:click
+>
+	<slot />
+</div>
+
+<style lang="scss">
+	.block-btn {
+		&:hover {
+			cursor: pointer;
+		}
+	}
+	.borderTop {
+		border-top: var(--border);
+	}
+	.borderBottom {
+		border-bottom: var(--border);
+	}
+</style>
