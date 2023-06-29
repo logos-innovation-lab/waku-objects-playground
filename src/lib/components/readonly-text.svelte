6d600a4db3f40dc00fe80774e5d603448a3f011f
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-06-29 16:02:43 -0400

    style: style overview state of Send Transaction

---
+++ src/lib/components/readonly-text.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	export let label: string | undefined = undefined
+	export let marginBottom = 24
+</script>
+
+<div class={`readonly`} style={`margin-bottom: ${marginBottom}px;`}>
+	{#if label}
+		<div class="label">
+			{label}
+		</div>
+	{/if}
+	<div class="main">
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	.readonly {
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-6);
+	}
+	.main {
+		background-color: var(--gray10);
+		padding: var(--spacing-12);
+		border-radius: var(--border-radius);
+		width: calc(100% + var(--spacing-24));
+		margin-left: calc(-1 * var(--spacing-12));
+		margin-right: calc(-1 * var(--spacing-12));
+	}
+
+	.label {
+		font-size: var(--font-size-sm);
+		color: var(--gray40);
+	}
+</style>
