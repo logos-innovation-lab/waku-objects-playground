3a68a4fe76889dc756c2940aaf03fd75ab3e434d
Attila Gazso <230163+agazso@users.noreply.github.com>
2023-07-12 18:25:33 +0200

    fix: add missing balance.ts

---
+++ src/lib/adapters/balance.ts
@@ -0,0 +1,49 @@
+import { balanceStore, type Token } from '$lib/stores/balances'
+import { defaultBlockchainNetwork, getBalance } from '$lib/adapters/transaction'
+
+export async function initializeBalances(address: string): Promise<void> {
+	balanceStore.update((state) => ({
+		...state,
+		loading: true,
+	}))
+
+	const nativeTokenAmount = await getBalance(address)
+
+	const ethData = {
+		...defaultBlockchainNetwork.nativeToken,
+		amount: nativeTokenAmount,
+	}
+
+	const daiData = {
+		name: 'Dai',
+		symbol: 'DAI',
+		decimals: 18,
+		amount: 7843900000000000000000n,
+		image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+	}
+
+	const balances = [ethData, daiData]
+	const balancesState = {
+		balances,
+		loading: false,
+	}
+	balanceStore.set(balancesState)
+}
+
+export async function checkBalance(address: string, token: Token): Promise<void> {
+	const nativeTokenAmount = await getBalance(address)
+
+	balanceStore.update((balanceState) => ({
+		...balanceState,
+		balances: balanceState.balances.map((value) => {
+			if (value.symbol !== token.symbol) {
+				return value
+			} else {
+				return {
+					...value,
+					amount: nativeTokenAmount,
+				}
+			}
+		}),
+	}))
+}
