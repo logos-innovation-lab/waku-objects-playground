f1b510a7d9376f82fa97afa84cd0d3e3429c2c45
Barbara Gomes <sugarpusher@users.noreply.github.com>
2023-07-13 08:59:20 -0400

    style: fix corners and spacing on chat bubbles (#180)
    Co-authored-by: Attila Gazso <230163+agazso@users.noreply.github.com>

--- src/lib/components/chat-message.svelte
+++ src/lib/components/chat-message.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
 	export let bubble = false
 	export let myMessage = false
+
+	const isFF = () => {
+		let browserInfo = navigator.userAgent
+		return browserInfo.includes('Firefox')
+	}
 </script>
 
-<div class={`message ${myMessage ? 'my-message' : 'their-message'}`}>
+<div class={`message ${myMessage ? 'my-message' : 'their-message'} ${isFF() ? 'ff' : ''}`}>
 	<div class={` ${bubble ? 'bubble message-content message-text text-lg' : ''}`}>
 		<slot />
 	</div>
@@ -39,11 +44,38 @@
 		font-style: italic;
 		margin-left: auto;
 		margin-right: 0;
+
+		//The + combinator matches the second element only if it immediately follows the first element.
+		& + .my-message:not(.ff) .message-text {
+			border-top-right-radius: 0;
+		}
+
+		//This combination matches the first element only if it immediately precedes the second element.
+		&:has(+ .my-message) {
+			margin-bottom: var(--spacing-6);
+
+			.message-text {
+				border-bottom-right-radius: 0;
+			}
+		}
 	}
 
 	.their-message {
 		align-items: flex-start;
 		text-align: left;
+
+		//The + combinator matches the second element only if it immediately follows the first element.
+		& + .their-message:not(.ff) .message-text {
+			border-top-left-radius: 0;
+		}
+
+		//This combination matches the first element only if it immediately precedes the second element.
+		&:has(+ .their-message) {
+			margin-bottom: var(--spacing-6);
+			.message-text {
+				border-bottom-left-radius: 0;
+			}
+		}
 
 		.message-content {
 			text-align: left;
