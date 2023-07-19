7aa165eb477940551b42484e70a8bbc7de090907
Barbara Gomes Carano <barbeegomes@gmail.com>
2023-07-19 09:50:03 -0400

    wip

--- src/routes/chat/[id]/+page.svelte
+++ src/routes/chat/[id]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { beforeUpdate, afterUpdate, onMount } from 'svelte'
+	import { beforeUpdate, afterUpdate, onMount, tick } from 'svelte'
 	import { page } from '$app/stores'
 
 	import Add from '$lib/components/icons/add.svelte'
@@ -13,16 +13,17 @@
 	import Avatar from '$lib/components/avatar.svelte'
 	import Dropdown from '$lib/components/dropdown.svelte'
 	import DropdownItem from '$lib/components/dropdown-item.svelte'
+	import ChatMessage from '$lib/components/chat-message.svelte'
+
 	import WakuObject from '$lib/objects/chat.svelte'
 
 	import { goto } from '$app/navigation'
+	import { browser } from '$app/environment'
 	import { chats } from '$lib/stores/chat'
+	import { profile } from '$lib/stores/profile'
+	import { walletStore } from '$lib/stores/wallet'
 	import adapters from '$lib/adapters'
 	import ROUTES from '$lib/routes'
-	import { walletStore } from '$lib/stores/wallet'
-	import { browser } from '$app/environment'
-	import { profile } from '$lib/stores/profile'
-	import ChatMessage from '$lib/components/chat-message.svelte'
 
 	let div: HTMLElement
 	let autoscroll = true
@@ -37,7 +38,6 @@
 
 	onMount(() => {
 		if (browser && div) {
-			console.log('onMount inner')
 			// It can not be done in onMount because the div is not yet rendered
 			setTimeout(() => {
 				div.scrollTo({
@@ -49,8 +49,12 @@
 	})
 
 	$: messages = $chats.chats.get($page.params.id)?.messages || []
+
 	let loading = false
 	let text = ''
+
+	const img = 'https://picsum.photos/500/700'
+
 	$: if (browser && !$walletStore.loading && $walletStore.wallet === undefined) goto(ROUTES.HOME)
 
 	const sendMessage = async () => {
@@ -64,6 +68,22 @@
 
 	$: chat = $chats.chats.get($page.params.id)
 	$: otherUser = chat?.users.find((m) => m.address !== $walletStore.wallet?.address)
+
+	function randomIntFromInterval(min: number, max: number) {
+		// min and max included
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+
+	// function isVertical() {
+	// 	try {
+	// 		// await tick() //waits until the dom has been updated
+	// 		// return img.clientHeight >= img.clientWidth
+	// 		console.log('done')
+	// 		return false
+	// 	} catch (error) {
+	// 		console.error(error)
+	// 	}
+	// }
 </script>
 
 {#if $walletStore.loading || $profile.loading}
@@ -96,12 +116,27 @@
 						<!-- Chat bubbles -->
 						{#each messages as message}
 							{#if message.type === 'user' && message.text.length > 0}
-								<ChatMessage
-									myMessage={message.fromAddress === $walletStore.wallet?.address ? true : false}
-									bubble
-								>
-									{@html message.text.replaceAll('\n', '</br>')}
-								</ChatMessage>
+								<div class="message-wrap">
+									<!-- TODO: add code to grab images -->
+									<div
+										class={`img-grid ${
+											message.fromAddress === $walletStore.wallet?.address ? 'my-message' : ''
+										}`}
+									>
+										{#each { length: randomIntFromInterval(0, 3) } as _}
+											<div>
+												<img class={`msg-img`} src={img} alt="message img" />
+												<div class="more text-sm">+17</div>
+											</div>
+										{/each}
+									</div>
+									<ChatMessage
+										myMessage={message.fromAddress === $walletStore.wallet?.address ? true : false}
+										bubble
+									>
+										{@html message.text.replaceAll('\n', '</br>')}
+									</ChatMessage>
+								</div>
 							{:else if message.type === 'data'}
 								<WakuObject {message} users={chat.users} />
 							{/if}
@@ -189,5 +224,82 @@
 		display: flex;
 		gap: var(--spacing-12);
 		align-items: center;
+	}
+
+	.message-wrap {
+		max-width: 100%;
+		display: flex;
+		flex-direction: column;
+		gap: var(--spacing-6);
+		margin-block: var(--spacing-6);
+	}
+
+	//The + combinator matches the second element only if it immediately follows the first element.
+	// .message-wrap + .message-wrap {
+	// 	margin-bottom: var(--spacing-12);
+	// }
+
+	.img-grid {
+		display: flex;
+		flex-direction: row;
+		gap: var(--spacing-6);
+		max-width: 75%;
+		justify-content: stretch;
+
+		div {
+			// flex-basis: 100%;
+			position: relative;
+
+			&:not(:only-child) {
+				:global(img) {
+					flex-basis: 100%;
+					aspect-ratio: 1;
+					object-fit: cover;
+					position: relative;
+					width: 100%;
+				}
+			}
+
+			&:only-child {
+				:global(img) {
+					max-width: 300px;
+					max-height: 300px;
+				}
+
+				// &.vertical {
+				// 	width: 80%;
+				// }
+			}
+
+			.more {
+				position: absolute;
+				background-color: var(--gray50);
+				color: var(--white);
+				border-radius: var(--border-radius-sm);
+				padding: 3px 6px;
+				text-align: center;
+				inset: var(--spacing-6) var(--spacing-6) auto auto;
+			}
+		}
+
+		&.my-message {
+			justify-content: flex-end;
+			margin-right: 0;
+			margin-left: auto;
+
+			> div:only-child :global(img) {
+				margin-left: auto;
+				margin-right: 0;
+			}
+		}
+
+		// :global(.msg-img:not(:only-child)) {
+		// 	flex-basis: 100%;
+		// 	aspect-ratio: 1;
+		// 	object-fit: cover;
+		// 	position: relative;
+		// 	width: 100%;
+		// 	// height: 100%;
+		// }
 	}
 </style>
