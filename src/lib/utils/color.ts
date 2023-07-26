79318315d40fbd3ac991a12c888640c7ae041600
Vojtech Simetka <vojtech@simetka.cz>
2023-07-26 14:14:05 +0200

    feat: dark mode color switching

--- src/lib/utils/color.ts
+++ src/lib/utils/color.ts
@@ -10,31 +10,31 @@ interface Color {
 // TODO: maybe these should actually be the other way around? So --white should actually be black and vice versa
 const darkColorVars: Color[] = [
 	{
-		name: '--color-base',
+		name: '--color-dark-base',
 		luminance: 1,
 	},
 	{
-		name: '--color-step-10',
+		name: '--color-dark-step-10',
 		luminance: 0.85,
 	},
 	{
-		name: '--color-step-20',
+		name: '--color-dark-step-20',
 		luminance: 0.6,
 	},
 	{
-		name: '--color-step-30',
+		name: '--color-dark-step-30',
 		luminance: 0.3,
 	},
 	{
-		name: '--color-step-40',
+		name: '--color-dark-step-40',
 		luminance: 0.03,
 	},
 	{
-		name: '--color-step-50',
+		name: '--color-dark-step-50',
 		luminance: 0.01,
 	},
 	{
-		name: '--color-accent',
+		name: '--color-dark-accent',
 		luminance: 0,
 	},
 ]
@@ -76,8 +76,11 @@ export function changeColors(baseColor: string, darkMode: DarkMode) {
 	const isDarkMode =
 		darkMode === 'dark' ||
 		(darkMode === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
-	const colors = isDarkMode ? darkColorVars : lightColorsVars
 
+	const colors = isDarkMode ? darkColorVars : lightColorsVars
+	const colorsToRemove = isDarkMode ? lightColorsVars : darkColorVars
+
+	// Define the correct variant of colors (either dark or light)
 	colors.forEach(({ name, luminance }) => {
 		const color = getClosestColor(baseColor, luminance)
 		const { r, g, b } = hexToRgb(color)
@@ -87,5 +90,11 @@ export function changeColors(baseColor: string, darkMode: DarkMode) {
 			`${name}-rgb`,
 			`${r.toFixed()}, ${g.toFixed()}, ${b.toFixed()}`,
 		)
+	})
+
+	// Remove the unused variant of colors (either dark or light)
+	colorsToRemove.forEach(({ name }) => {
+		document.documentElement.style.removeProperty(name)
+		document.documentElement.style.removeProperty(`${name}-rgb`)
 	})
 }
