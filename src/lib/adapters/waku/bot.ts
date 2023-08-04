d5f27c265c6ba1e6427995eed4b925cf6b8cfc3e
Attila Gazso <230163+agazso@users.noreply.github.com>
2023-08-04 15:29:25 +0200

    chore: cleanup

--- src/lib/adapters/waku/bot.ts
+++ src/lib/adapters/waku/bot.ts
@@ -9,21 +9,11 @@ async function main() {
 	console.log({ waku })
 
 	const botAddress = '0x1190ff60748db04e0457593c94219ccf739374d5'
-	// const yourAddress = '0xf24e1D920d552AF5075163238135850AA8ea4eB7'
-
 	const botProfile = {
 		name: 'Bot',
 		avatar: 'QmahJdru5ooiPrn8FipC7tLb2t9o39Kdszohk2g5SFffnQ', // IPFS hash of image comes here
 	}
 	await storeDocument(waku, 'profile', botAddress, botProfile)
-
-	// const text = 'hello, I am Bot, ask me anything'
-	// await sendMessage(waku, yourAddress, {
-	// 	type: 'user',
-	// 	timestamp: Date.now(),
-	// 	text,
-	// 	fromAddress: botAddress,
-	// })
 
 	await subscribe(waku, 'private-message', botAddress, async (msg) => {
 		const decodedPayload = decodeMessagePayload(msg)
