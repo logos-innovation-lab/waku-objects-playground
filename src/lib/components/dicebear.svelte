567a937571ae0217bee4fb858f761cc93ca4fb7c
Attila Gazso <230163+agazso@users.noreply.github.com>
2023-08-14 23:01:43 +0200

    fix: add missing dicebear component

---
+++ src/lib/components/dicebear.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import User from './icons/user.svelte'
+
+	export let name: string
+	export let size = 48
+	export let style = localStorage.getItem('dicebearStyle') || 'avataaars-neutral'
+	export let scale = 75
+	export let radius = 50
+
+	const seed = name
+	const avatarSize = size / 2
+</script>
+
+{#if style === 'user'}
+	<User size={avatarSize} />
+{:else}
+	<img
+		alt="avatar"
+		src="https://api.dicebear.com/6.x/{style}/svg?seed={seed}&size={size}&radius={radius}&scale={scale}"
+	/>
+{/if}
