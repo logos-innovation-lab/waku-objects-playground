573a55bc65a0e97ca568f55116164fa1734b9fe7
Vojtech Simetka <vojtech@simetka.cz>
2023-09-12 11:08:34 +0200

    revert: use @sveltejs/adapter-auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-auto'
 import preprocessor from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
