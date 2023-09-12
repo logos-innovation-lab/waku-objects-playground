69c0153cdaa761922e8ca37f55684532914bbd1a
Vojtech Simetka <vojtech@simetka.cz>
2023-09-12 11:35:07 +0200

    fix: waku@0.18 build error by using polyfills

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,18 @@
 import adapter from '@sveltejs/adapter-auto'
 import preprocessor from 'svelte-preprocess'
+import { sveltekit } from '@sveltejs/kit/vite'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	plugins: [
+		sveltekit(),
+		nodePolyfills({
+			// Whether to polyfill `node:` protocol imports.
+			protocolImports: true,
+		}),
+	],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: preprocessor(),
@@ -12,6 +22,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+	},
+	optimizeDeps: {
+		include: ['buffer', 'process'],
 	},
 }
 
