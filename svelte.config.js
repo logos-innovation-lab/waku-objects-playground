1acc88c2ac5c3f20baa39d4fc88c10e519c3ed8c
Vojtech Simetka <vojtech@simetka.cz>
2023-10-27 12:14:27 +0200

    build: add mechanism to build SPA version of the app (#481)

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-auto'
+import adapterStatic from '@sveltejs/adapter-static'
 import preprocessor from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: process.env.STATIC
+			? adapterStatic({
+					// default options are shown
+					pages: 'build',
+					assets: 'build',
+					fallback: 'index.html',
+			  })
+			: adapter(),
 	},
 }
 
