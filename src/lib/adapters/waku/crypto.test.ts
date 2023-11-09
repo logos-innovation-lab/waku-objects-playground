a26b93fcad0bf1fd51f4dec77b4daa37f592651f
Attila Gazso <230163+agazso@users.noreply.github.com>
2023-11-09 17:41:01 +0100

    fix: remove unused crypto code

--- src/lib/adapters/waku/crypto.test.ts
+++ src/lib/adapters/waku/crypto.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from 'vitest'
 import { publicKeyToAddress } from './crypto'
 
-const testPrivateKey = 'd195918969e09d9394c768e25b621eafc4c360117a9e1eebb0a68bfd53119ba4'
+// const testPrivateKey = 'd195918969e09d9394c768e25b621eafc4c360117a9e1eebb0a68bfd53119ba4'
 const testCompressedPublicKey = '0374a6b1cea74a7a755396d8c62a3be4eb9098c7bb286dcdfc02ab93e7683c93f9'
 const testUncompressedPublicKey =
 	'0474a6b1cea74a7a755396d8c62a3be4eb9098c7bb286dcdfc02ab93e7683c93f99515f1cf8980e0cb25b6078113813d90d99303aaea1aa34c12805f8355768e21'
