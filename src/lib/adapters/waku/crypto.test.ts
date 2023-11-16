49a6a0ce20999838c552d26df393ec3112f33383
Attila Gazso <230163+agazso@users.noreply.github.com>
2023-11-16 11:24:26 +0100

    feat: invite with ECIES (#496)

--- src/lib/adapters/waku/crypto.test.ts
+++ src/lib/adapters/waku/crypto.test.ts
@@ -1,7 +1,12 @@
 import { describe, it, expect } from 'vitest'
-import { publicKeyToAddress } from './crypto'
+import {
+	compressPublicKey,
+	privateKeyToPublicKey,
+	publicKeyToAddress,
+	uncompressPublicKey,
+} from './crypto'
 
-// const testPrivateKey = 'd195918969e09d9394c768e25b621eafc4c360117a9e1eebb0a68bfd53119ba4'
+const testPrivateKey = 'd195918969e09d9394c768e25b621eafc4c360117a9e1eebb0a68bfd53119ba4'
 const testCompressedPublicKey = '0374a6b1cea74a7a755396d8c62a3be4eb9098c7bb286dcdfc02ab93e7683c93f9'
 const testUncompressedPublicKey =
 	'0474a6b1cea74a7a755396d8c62a3be4eb9098c7bb286dcdfc02ab93e7683c93f99515f1cf8980e0cb25b6078113813d90d99303aaea1aa34c12805f8355768e21'
@@ -16,5 +21,24 @@ describe('publicKeyToAddress', () => {
 	it('calculates correct address for uncompressed public key', () => {
 		const address = publicKeyToAddress(testUncompressedPublicKey)
 		expect(address).toEqual(testAddress)
+	})
+})
+
+describe('public key compression', () => {
+	it('calculates compressed public key', () => {
+		const result = compressPublicKey(testUncompressedPublicKey)
+		expect(result).toEqual(testCompressedPublicKey)
+	})
+
+	it('calculates uncompressed public key', () => {
+		const result = uncompressPublicKey(testCompressedPublicKey)
+		expect(result).toEqual(testUncompressedPublicKey)
+	})
+})
+
+describe('privateKeyToPublicKey', () => {
+	it('returns the public key', () => {
+		const publicKey = privateKeyToPublicKey(testPrivateKey)
+		expect(publicKey).toEqual(testCompressedPublicKey)
 	})
 })
